b499b24902913450b8896506cfc6512b49df0957
Tubaleviao <alvaro.prata.filho@gmail.com>
2020-03-19 13:55:29 +0000

    new function, smooth movement

--- app.js
+++ app.js
@@ -1,89 +1,91 @@
 const gpio = require('pigpio').Gpio
+let mode = {mode: gpio.OUTPUT}
 
 let max = 2500 // max angle: 2500
 let min = 500 // min angle: 500
-let right_feet_pin = 2
-let right_knee_pin = 3
-let right_thigh_pin = 4
-let right_hip_pin = 5
-let left_feet_pin = 6
-let left_knee_pin = 7
-let left_thigh_pin = 8
-let left_hip_pin = 9
-let neck_lr_pin = 10
-let neck_ud_pin = 11
 
-const right_feet = new gpio(right_feet_pin, {mode: gpio.OUTPUT})
-const right_knee = new gpio(right_knee_pin, {mode: gpio.OUTPUT})
-const right_thigh = new gpio(right_thigh_pin, {mode: gpio.OUTPUT})
-const right_hip = new gpio(right_hip_pin, {mode: gpio.OUTPUT})
-const left_feet = new gpio(left_feet_pin, {mode: gpio.OUTPUT})
-const left_knee = new gpio(left_knee_pin, {mode: gpio.OUTPUT})
-const left_thigh = new gpio(left_thigh_pin, {mode: gpio.OUTPUT})
-const left_hip = new gpio(left_hip_pin, {mode: gpio.OUTPUT})
-const lr_neck = new gpio(neck_lr_pin, {mode: gpio.OUTPUT})
-const ud_neck = new gpio(neck_ud_pin, {mode: gpio.OUTPUT})
+const right_feet = {pin: 2, lib: new gpio(2, mode), position: 1400, default: 1400}
+const right_knee = {pin: 3, lib: new gpio(3, mode), position: 600, default: 600}
+const right_thigh = {pin: 4, lib: new gpio(4, mode), position: 1600, default: 1600}
+const right_hip = {pin: 5, lib: new gpio(5, mode), position: 1400, default: 1400}
+const left_feet = {pin: 6, lib: new gpio(6, mode), position: 1550, default: 1550}
+const left_knee = {pin: 7, lib: new gpio(7, mode), position: 2400, default: 2400}
+const left_thigh = {pin: 8, lib: new gpio(8, mode), position: 1500, default: 1500}
+const left_hip = {pin: 9, lib: new gpio(9, mode), position: 1700, default: 1700}
+const lr_neck = {pin: 10, lib: new gpio(10, mode), position: 1400, default: 1400}
+const ud_neck = {pin: 11, lib: new gpio(11, mode), position: 1900, default: 1900}
 
+right_feet.lib.servoWrite(right_feet.position)
+right_knee.lib.servoWrite(right_knee.position)
+right_thigh.lib.servoWrite(right_thigh.position)
+right_hip.lib.servoWrite(right_hip.position)
+left_feet.lib.servoWrite(left_feet.position)
+left_knee.lib.servoWrite(left_knee.position)
+left_thigh.lib.servoWrite(left_thigh.position)
+left_hip.lib.servoWrite(left_hip.position)
+lr_neck.lib.servoWrite(lr_neck.position)
+ud_neck.lib.servoWrite(ud_neck.position)
 
-// defaults (standing)
-let rf_p = 1450 // 2 (+ = up)
-let rk_p = 500 // 3
-let rt_p = 1700 // 4
-let rh_p = 1300 // 5
-let lf_p = 1550 // 6 (- = up)
-let lk_p = 2400 // 7
-let lt_p = 1500 // 8
-let lh_p = 1600 // 9
-let lrn_p = 1200 // 10
-let upn_p = 1900 // 11
-
-right_feet.servoWrite(rf_p) 
-right_knee.servoWrite(rk_p) 
-right_thigh.servoWrite(rt_p) 
-right_hip.servoWrite(rh_p) 
-left_feet.servoWrite(lf_p) 
-left_knee.servoWrite(lk_p) 
-left_thigh.servoWrite(lt_p) 
-left_hip.servoWrite(lh_p) 
-lr_neck.servoWrite(lrn_p) 
-ud_neck.servoWrite(upn_p) 
-
-let move = (servo, position, angle, max_vel=0) => {
+let move = (obj, angle, max_vel=0) => {
 	if(max_vel == 0){
-		let inc = (angle-position)/100
-		let movement = setInterval(fu, 20)
+		let inc = (angle-obj.position)/100
+		let movement = setInterval(fu, 20)		
 		function fu(){
-			if(position == angle){
+			if(obj.position == angle){
 				clearInterval(movement);
 			}else{
-				position = position+inc
-				//console.log(Math.floor(position))
-				servo.servoWrite(Math.floor(position))
+				obj.position = obj.position+inc
+				obj.lib.servoWrite(Math.floor(obj.position))
 			}
 		}
 	}
 }
 
+let move2 = async (obj, destiny, d2=0) => {
+	
+	let inc = (destiny-obj.position)/100
+	let old_position = obj.position+0
+	let first = true
+	let initial = obj.position+0
+	const mid = ((destiny-initial)/2)+ initial
+	const variance = 40
+	const delay = 7
+	async function go(){
+		if(obj.position != destiny){
+			if(first || old_position != obj.position){
+				let new_time = Math.floor(delay+(variance*Math.abs(1-(obj.position/mid))))
+				setTimeout(fu, new_time)
+				old_position = obj.position+0
+				first = false
+			}
+		}
+	}
+	async function fu(){
+		obj.lib.servoWrite(Math.floor(obj.position))
+		obj.position = obj.position+inc
+		setTimeout(go, d2)
+	}
+	go()
+	
+}
+
 let goDown = (angle=400) => {
-	move(right_feet, rf_p, rf_p+angle) // (angle-(angle-100)*0.2)
-	move(right_knee, rk_p, rk_p+(200+angle*2))
-	move(right_thigh, rt_p, rt_p-(200+angle*2))
-	move(left_feet, lf_p, lf_p-angle)
-	move(left_knee, lk_p, lk_p-(200+angle*2))
-	move(left_thigh, lt_p, lt_p+(200+angle*2))
+	move2(right_feet, right_feet.position+angle)
+	move2(left_feet, left_feet.position-angle, 5)
+	move2(right_knee, right_knee.position+(200+angle*2))
+	move2(left_knee, left_knee.position-(200+angle*2), 5)
+	move2(right_thigh, right_thigh.position-(200+angle*2))
+	move2(left_thigh, left_thigh.position+(200+angle*2) , 5)
 }
 
 let goUp = (angle=400) => {
-	move(right_feet, rf_p+angle, rf_p) // for each 100 here
-	move(right_knee, rk_p+(200+angle*2), rk_p) // increase 200 here
-	move(right_thigh, rt_p-(200+angle*2), rt_p) 
-	move(left_feet, lf_p-angle, lf_p) 
-	move(left_knee, lk_p-(200+angle*2), lk_p) 
-	move(left_thigh, lt_p+(200+angle*2), lt_p) 
+	move2(right_feet, right_feet.default) // for each 100 here
+	move2(left_feet, left_feet.default, 5) 
+	move2(right_knee, right_knee.default) // increase 200 here
+	move2(left_knee, left_knee.default, 5) 
+	move2(right_thigh, right_thigh.default) 
+	move2(left_thigh, left_thigh.default, 5) 
 }
 
 setTimeout(goDown, 3000)
 setTimeout(goUp, 6000)
-
-//setTimeout(goReallyDown, 3000)
-//setTimeout(goReallyUp, 6000)
